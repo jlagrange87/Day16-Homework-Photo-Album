381de697f14567c715e9a8db9f59f434b12d704d
Joshua Lagrange <jlagrange87@gmail.com>
2015-05-30 14:09:13 -0500

    Fixed a few text errors on page

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3,22 +3,22 @@ $(document).ready(function(){
   	$('.menu').animate({
   		top: "0px",
      	left: "0px"
-   	}, 200);
+   	}, 400);
 		$('body').animate({
      	marginLeft: "195px"
-  	}, 200);
-    $("#album-container").hide(200);
+  	}, 400);
+    $("#album-container").hide(400);
  	});
 
  	$('.icon-close').click(function() {
    	$('.menu').animate({
     	left: "-195px"
-   	}, 200);
+   	}, 400);
 		$('body').animate({
     	marginLeft: "0px",
       marginRight: "0px"
-  	}, 200);
-    $("header").html("My Photo Album")
+  	}, 400);
+    $("header").html("My Photo Album").css("color", "white").css("background-color","#333333");
     $("#album-container").show(200);
     $("#album1").hide(200);
     $("#album2").hide(200);
@@ -28,7 +28,7 @@ $(document).ready(function(){
     $("#album6").hide(200);
 	});
   $(".album1").click(function(){
-    $("header").html("Kids and I")
+    $("header").html("Kids and I").css("color", "black").css("background-color","white");
     $("#album1").show(200);
     $("#album2").hide(200);
     $("#album3").hide(200);
@@ -37,7 +37,7 @@ $(document).ready(function(){
     $("#album6").hide(200);
   })
   $(".album2").click(function(){
-    $("header").html("Neat Stuff")
+    $("header").html("Neat Stuff").css("color", "black").css("background-color","white");
     $("#album1").hide(200);
     $("#album2").show(200);
     $("#album3").hide(200);
@@ -46,7 +46,7 @@ $(document).ready(function(){
     $("#album6").hide(200);
   })
   $(".album3").click(function(){
-    $("header").html("Beautiful Wife")
+    $("header").html("Beautiful Wife").css("color", "black").css("background-color","white");
     $("#album1").hide(200);
     $("#album2").hide(200);
     $("#album3").show(200);
@@ -55,7 +55,7 @@ $(document).ready(function(){
     $("#album6").hide(200);
   })
   $(".album4").click(function(){
-    $("header").html("The Wife and I")
+    $("header").html("The Wife and I").css("color", "black").css("background-color","white");
     $("#album1").hide(200);
     $("#album2").hide(200);
     $("#album3").hide(200);
@@ -64,7 +64,7 @@ $(document).ready(function(){
     $("#album6").hide(200);
   })
   $(".album5").click(function(){
-    $("header").html("Just the Kids")
+    $("header").html("Just the Kids").css("color", "black").css("background-color","white");
     $("#album1").hide(200);
     $("#album2").hide(200);
     $("#album3").hide(200);
@@ -73,7 +73,7 @@ $(document).ready(function(){
     $("#album6").hide(200);
   })
   $(".album6").click(function(){
-    $("header").html("Holidays")
+    $("header").html("Holidays").css("color", "black").css("background-color","white");
     $("#album1").hide(200);
     $("#album2").hide(200);
     $("#album3").hide(200);
