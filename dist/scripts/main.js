bf9c89130ac3d5883542bb4fb524e9d087074103
Joshua Lagrange <jlagrange87@gmail.com>
2015-05-31 22:56:33 -0500

    RePushing for good measure

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -29,9 +29,9 @@ $(document).ready(function(){
 	});
   $(".album1").click(function(){
     $("header").html("Kids and I").css("color", "black").css("background-color","white");
-    $(".photo1a").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
-    $(".photo2a").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(300);
-    $(".photo3a").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
+    $(".photo1a").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
+    $(".photo2a").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(200);
+    $(".photo3a").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
     $("#album1").show(200);
     $("#album2").hide(200);
     $("#album3").hide(200);
@@ -41,9 +41,9 @@ $(document).ready(function(){
   })
   $(".album2").click(function(){
     $("header").html("Neat Stuff").css("color", "black").css("background-color","white");
-    $(".photo1b").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(300);
-    $(".photo2b").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
-    $(".photo3b").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
+    $(".photo1b").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(200);
+    $(".photo2b").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
+    $(".photo3b").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
     $("#album1").hide(200);
     $("#album2").show(200);
     $("#album3").hide(200);
@@ -53,9 +53,9 @@ $(document).ready(function(){
   })
   $(".album3").click(function(){
     $("header").html("Beautiful Wife").css("color", "black").css("background-color","white");
-    $(".photo1c").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
-    $(".photo2c").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(300);
-    $(".photo3c").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(300);
+    $(".photo1c").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
+    $(".photo2c").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(200);
+    $(".photo3c").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(200);
     $("#album1").hide(200);
     $("#album2").hide(200);
     $("#album3").show(200);
@@ -65,9 +65,9 @@ $(document).ready(function(){
   })
   $(".album4").click(function(){
     $("header").html("The Wife and I").css("color", "black").css("background-color","white");
-    $(".photo1d").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(300);
-    $(".photo2d").hide().removeClass("col-sm-offset-4").removeClass("col-sm-4").addClass("col-sm-4").show(300);
-    $(".photo3d").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(300);
+    $(".photo1d").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(200);
+    $(".photo2d").hide().removeClass("col-sm-offset-4").removeClass("col-sm-4").addClass("col-sm-4").show(200);
+    $(".photo3d").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show(200);
     $("#album1").hide(200);
     $("#album2").hide(200);
     $("#album3").hide(200);
@@ -77,9 +77,9 @@ $(document).ready(function(){
   })
   $(".album5").click(function(){
     $("header").html("Just the Kids").css("color", "black").css("background-color","white");
-    $(".photo1e").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
-    $(".photo2e").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
-    $(".photo3e").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
+    $(".photo1e").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
+    $(".photo2e").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
+    $(".photo3e").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
     $("#album1").hide(200);
     $("#album2").hide(200);
     $("#album3").hide(200);
@@ -89,9 +89,9 @@ $(document).ready(function(){
   })
   $(".album6").click(function(){
     $("header").html("Holidays").css("color", "black").css("background-color","white");
-    $(".photo1f").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
-    $(".photo2f").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
-    $(".photo3f").hide().removeClass("col-sm-offset-1").removeClass("col-sm-10").addClass("col-sm-4").show(300);
+    $(".photo1f").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
+    $(".photo2f").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
+    $(".photo3f").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show(200);
     $("#album1").hide(200);
     $("#album2").hide(200);
     $("#album3").hide(200);
@@ -100,94 +100,318 @@ $(document).ready(function(){
     $("#album6").show(200);
   })
   $(".photo1a").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo2a").hide();
     $(".photo3a").hide();
-    $(".photo1a").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo1a").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
   $(".photo2a").click(function(){
+      $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1a").hide();
     $(".photo3a").hide();
-    $(".photo2a").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(300);
+    $(".photo2a").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(200);
   })
   $(".photo3a").click(function(){
+      $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1a").hide();
     $(".photo2a").hide();
-    $(".photo3a").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo3a").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
   $(".photo1b").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo2b").hide();
     $(".photo3b").hide();
-    $(".photo1b").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(300);
+    $(".photo1b").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(200);
   })
   $(".photo2b").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1b").hide();
     $(".photo3b").hide();
-    $(".photo2b").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo2b").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
   $(".photo3b").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1b").hide();
     $(".photo2b").hide();
-    $(".photo3b").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo3b").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
   $(".photo1c").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo2c").hide();
     $(".photo3c").hide();
-    $(".photo1c").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo1c").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
   $(".photo2c").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1c").hide();
     $(".photo3c").hide();
-    $(".photo2c").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(300);
+    $(".photo2c").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(200);
   })
   $(".photo3c").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1c").hide();
     $(".photo2c").hide();
-    $(".photo3c").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(300);
+    $(".photo3c").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(200);
   })
   $(".photo1d").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo2d").hide();
     $(".photo3d").hide();
-    $(".photo1d").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(300);
+    $(".photo1d").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(200);
   })
   $(".photo2d").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1d").hide();
     $(".photo3d").hide();
-    $(".photo2d").hide().removeClass("col-sm-4").addClass("col-sm-4").addClass("col-sm-offset-4").show(300);
+    $(".photo2d").hide().removeClass("col-sm-4").addClass("col-sm-4").addClass("col-sm-offset-4").show(200);
   })
   $(".photo3d").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1d").hide();
     $(".photo2d").hide();
-    $(".photo3d").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(300);
+    $(".photo3d").hide().removeClass("col-sm-4").addClass("col-sm-6").addClass("col-sm-offset-3").show(200);
   })
   $(".photo1e").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo2e").hide();
     $(".photo3e").hide();
-    $(".photo1e").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo1e").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
   $(".photo2e").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1e").hide();
     $(".photo3e").hide();
-    $(".photo2e").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo2e").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
   $(".photo3e").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1e").hide();
     $(".photo2e").hide();
-    $(".photo3e").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo3e").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
   $(".photo1f").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo2f").hide();
     $(".photo3f").hide();
-    $(".photo1f").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo1f").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
   $(".photo2f").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1f").hide();
     $(".photo3f").hide();
-    $(".photo2f").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo2f").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
   $(".photo3f").click(function(){
+    $('.menu').animate({
+      left: "-195px"
+    }, 200);
+    $('body').animate({
+      marginLeft: "0px",
+      marginRight: "0px"
+    }, 200);
+    $(".btn").show();
     $(".photo1f").hide();
     $(".photo2f").hide();
-    $(".photo3f").hide().removeClass("col-sm-4").addClass("col-sm-10").addClass("col-sm-offset-1").show(300);
+    $(".photo3f").hide().removeClass("col-sm-4").addClass("col-sm-8").addClass("col-sm-offset-2").show(200);
   })
-	
+ $(".btn1").click(function(){
+    $(".menu").animate({
+      top: "0px",
+      left: "0px"
+    }, 200);
+    $("body").animate({
+      marginLeft: "195px"
+    }, 200);
+    $(".photo1a").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".photo2a").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show();
+    $(".photo3a").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".btn").hide();
+  });
+  $(".btn2").click(function(){
+      $(".menu").animate({
+      top: "0px",
+      left: "0px"
+    }, 200);
+    $("body").animate({
+      marginLeft: "195px"
+    }, 200);
+    $(".photo1b").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show();
+    $(".photo2b").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".photo3b").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".btn").hide();
+  });
+  $(".btn3").click(function(){
+      $(".menu").animate({
+      top: "0px",
+      left: "0px"
+    }, 200);
+    $("body").animate({
+      marginLeft: "195px"
+    }, 200);
+    $(".photo1c").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".photo2c").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show();
+    $(".photo3c").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show();
+    $(".btn").hide();
+  });
+  $(".btn4").click(function(){
+      $(".menu").animate({
+      top: "0px",
+      left: "0px"
+    }, 200);
+    $("body").animate({
+      marginLeft: "195px"
+    }, 200);
+    $(".photo1d").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show();
+    $(".photo2d").hide().removeClass("col-sm-offset-4").removeClass("col-sm-4").addClass("col-sm-4").show();
+    $(".photo3d").hide().removeClass("col-sm-offset-3").removeClass("col-sm-6").addClass("col-sm-4").show();
+    $(".btn").hide();
+  });
+  $(".btn5").click(function(){
+      $(".menu").animate({
+      top: "0px",
+      left: "0px"
+    }, 200);
+    $("body").animate({
+      marginLeft: "195px"
+    }, 200);
+    $(".photo1e").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".photo2e").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".photo3e").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".btn").hide();
+  });
+  $(".btn6").click(function(){
+      $(".menu").animate({
+      top: "0px",
+      left: "0px"
+    }, 200);
+    $("body").animate({
+      marginLeft: "195px"
+    }, 200);
+    $(".photo1f").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".photo2f").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".photo3f").hide().removeClass("col-sm-offset-2").removeClass("col-sm-8").addClass("col-sm-4").show();
+    $(".btn").hide();
+  });
 });
+
+
+    
